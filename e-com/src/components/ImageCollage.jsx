5ec350d16909838d96c5b82dd1869f78885ee655
pooja12-dev <harpalanipooja19@gmail.com>
2025-04-22 16:30:52 +0530

    made image collage responsivee

--- e-com/src/components/ImageCollage.jsx
+++ e-com/src/components/ImageCollage.jsx
@@ -2,8 +2,20 @@ import React from "react";
 
 function ImageCollage() {
   return (
-    <div className="relative w-full flex flex-wrap justify-center items-start gap-x-3 gap-y-4 py-6 md:py-12">
-      <img src="/ornaments.png" alt="Ornaments" />
+    <div className="h-screen min-h-[360px] max-h-[700px] w-full overflow-hidden">
+      {/* Image for large and medium screens */}
+      <img
+        src="/ornaments.png"
+        alt="Ornaments for large screens"
+        className="hidden md:block w-full  md:mx-2 h-full "
+      />
+
+      {/* Image for small screens */}
+      <img
+        src="/ornaments-sm.webp"
+        alt="Ornaments for small screens"
+        className="block md:hidden w-full h-full object-cover"
+      />
     </div>
   );
 }
